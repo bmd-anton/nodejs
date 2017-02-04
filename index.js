3f1546fbf925d318aab11794376b9ec3dc8abff2
rony.setyawan <rony.setyawan.st@gmail.com>
2017-02-04 23:45:46 +0700

    add lodash

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // index.js
 
 require('./app/index') 
- 
-console.log(`Hello Brother!!!`)
+
+const _ = require('lodash')
+
+const name = _.assign({ 'firstname': 'rony' }, { 'lastname': 'setyawan' })
+console.log(`Hello Brother ${name.firstname} ${name.lastname} !!!`)
